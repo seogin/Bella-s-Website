f3cbe96df068edc95ce5aae22906881145c0a02e
seogin <hsihsj94@gmail.com>
2024-07-22 17:03:49 -0700

    Reset local storage after one hour

--- toggle.js
+++ toggle.js
@@ -44,7 +44,7 @@ function reset_localstorage() {
   if (setupTime == null) {
     localStorage.setItem('setupTime', now)
   } else {
-    if (now - setupTime > hours * 60 * 1000) {
+    if (now - setupTime > hours * 60 * 60 * 1000) {
       localStorage.clear();
       localStorage.setItem('setupTime', now);
     }
